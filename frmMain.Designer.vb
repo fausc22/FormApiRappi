c744dfc12e392cc3d97f8443b6b2dba1e5361997
faaus <faaus@DESKTOP-MT1QK8H>
2023-05-19 19:54:28 -0300

    Agregar archivos de proyecto.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cmdEnviar = New Button()
+        lblResultado = New Label()
+        txtResultado = New TextBox()
+        SuspendLayout()
+        ' 
+        ' cmdEnviar
+        ' 
+        cmdEnviar.Location = New Point(128, 42)
+        cmdEnviar.Name = "cmdEnviar"
+        cmdEnviar.Size = New Size(75, 23)
+        cmdEnviar.TabIndex = 0
+        cmdEnviar.Text = "Enviar"
+        cmdEnviar.UseVisualStyleBackColor = True
+        ' 
+        ' lblResultado
+        ' 
+        lblResultado.AutoSize = True
+        lblResultado.Location = New Point(316, 175)
+        lblResultado.Name = "lblResultado"
+        lblResultado.Size = New Size(0, 15)
+        lblResultado.TabIndex = 1
+        ' 
+        ' txtResultado
+        ' 
+        txtResultado.Location = New Point(67, 82)
+        txtResultado.Name = "txtResultado"
+        txtResultado.ReadOnly = True
+        txtResultado.Size = New Size(209, 23)
+        txtResultado.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(330, 145)
+        Controls.Add(txtResultado)
+        Controls.Add(lblResultado)
+        Controls.Add(cmdEnviar)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents cmdEnviar As Button
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents txtResultado As TextBox
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,69 @@
+﻿Imports System.IO
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Text
+
+
+Public Class Form1
+
+    Private Async Sub cmdEnviar_Click(sender As Object, e As EventArgs) Handles cmdEnviar.Click
+        ' Abrir dialogo para seleccionar JSON
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Archivos JSON (*.json)|*.json"
+        openFileDialog.Title = "Seleccionar archivo JSON"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                ' Leer el contenido del JSON
+                Dim json As String = Await File.ReadAllTextAsync(openFileDialog.FileName)
+
+                'Enviar el JSON a la api
+                Dim respuesta As String = Await EnviarJSONAsync(json)
+
+                'Mostrar la respuesta en label
+                txtResultado.Text = respuesta
+
+            Catch ex As Exception
+                MessageBox.Show("Error al cargar y enviar el archivo JSON: " & ex.Message)
+            End Try
+        End If
+
+
+    End Sub
+
+
+    ' procedimiento para enviar JSON
+    Private Async Function EnviarJSONAsync(json As String) As Task(Of String)
+        'URL de la API
+        Dim url As String = "http://localhost/POST.php"
+
+        Try
+            'Crear HTTP
+            Using client As New HttpClient()
+                client.Timeout = TimeSpan.FromMinutes(15)
+                client.DefaultRequestHeaders.Accept.Clear()
+                client.DefaultRequestHeaders.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+
+                'contenido de la solicitud
+                Dim content As New StringContent(json, Encoding.UTF8, "application/json")
+
+
+                'enviar solicitud POST a la API y obtener respuesta
+                Dim response As HttpResponseMessage = Await client.PostAsync(url, content)
+
+                ' Leer respuesta
+                Dim respuesta As String = Await response.Content.ReadAsStringAsync()
+
+                Return respuesta
+
+            End Using
+        Catch ex As Exception
+            Throw New Exception("Error al enviar el JSON a la API: " & ex.Message)
+
+        End Try
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
